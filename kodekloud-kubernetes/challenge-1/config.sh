aa01384cc40b413e2c57e9ab88149250ed308675
Raihanarfi12 <onlyraihan00@gmail.com>
2025-02-24 15:03:05 +0700

    Fix jekyll pod pvc and config.sh

--- kodekloud-kubernetes/challenge-1/config.sh
+++ kodekloud-kubernetes/challenge-1/config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-kubectl apply -f *.yaml
-kubectl config set-context --user=martin --cluster=kubernetes
+kubectl apply -f developer-role.yaml
+kubectl apply -f developer-role-binding.yaml
+kubectl apply -f jekyll-pvc.yaml
+kubectl apply -f jekyll.yaml
+kubectl apply -f jekyll-node-services.yaml
+kubectl config set-credentials martin --client-key=/root/martin.key --client-certificate=/root/martin.crt --embed-certs=false
+kubectl config set-context developer --cluster=kubernetes --user=martin
+
