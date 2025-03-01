6e567ca2d22bb1121b68f4acac8f18734a015b72
Raihanarfi12 <onlyraihan00@gmail.com>
2025-03-01 11:57:49 +0700

    Finish challenge-1 kubernetes kodekloud

--- kodekloud-kubernetes/challenge-1/config.sh
+++ kodekloud-kubernetes/challenge-1/config.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-kubectl apply -f *.yaml
-kubectl config set-context --user=martin --cluster=kubernetes
+kubectl apply -f developer-role.yaml
+kubectl apply -f developer-role-binding.yaml
+kubectl apply -f jekyll-pvc.yaml
+kubectl apply -f jekyll.yaml
+kubectl apply -f jekyll-node-services.yaml
+
+#for kubeconfig context
+kubectl config set-credentials martin \
+  --client-key=/root/martin.key \
+  --client-certificate=/root/martin.crt \
+  --embed-certs=false
+
+kubectl config set-context developer \
+  --cluster=kubernetes \
+  --user=martin
